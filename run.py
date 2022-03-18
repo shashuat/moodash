5f5ee5e329b0eebf066e6cda85d9370be87fd7db
rhythmrx9 <rhythmrx9@gmail.com>
2022-03-18 10:43:32 +0530

    changes to ui

--- run.py
+++ run.py
@@ -2,5 +2,7 @@ from flaskblog import create_app
 
 app = create_app()
 
-if __name__ == "__main__":   # run module directly, no need for python -m flask run, and env vars
+if (
+    __name__ == "__main__"
+):  # run module directly, no need for python -m flask run, and env vars
     app.run(debug=True)
